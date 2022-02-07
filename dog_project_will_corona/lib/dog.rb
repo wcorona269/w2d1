370cd185e5b064996083ea64a645309cc3e79cfb
wcorona269 <wcorona269@gmail.com>
2022-02-07 16:45:30 -0500

    First comit

---
+++ dog_project_will_corona/lib/dog.rb
@@ -0,0 +1,47 @@
+class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(num)
+        @age = num
+    end
+
+    def bark
+        if @age > 3
+            return @bark.upcase
+        else
+            return @bark.downcase
+        end
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(string)
+        favorite_foods.each do |food|
+            if food.downcase.include?(string.downcase)
+                return true
+            end
+        end
+        
+        return false
+    end
+end
